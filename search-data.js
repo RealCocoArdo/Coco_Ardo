4956a9fcd140a29dde9e1529c27560defa0adc25
Coco_Ardo <105318683+RealCocoArdo@users.noreply.github.com>
2025-08-22 12:53:48 +0200

    added small searchengine

---
+++ search-data.js
@@ -0,0 +1,97 @@
+const searchData = [
+
+//  Bitcoin Knowledge
+  {
+    title: "Origins of money",
+    url: "https://nakamotoinstitute.org/shelling-out/",
+    content: "History excurs the evolution of money",
+    tags: ["Coco_Ardo","History","evolution"]
+  },
+  {
+    title: "Bitcoin Whitepaper",
+    url: "https://bitcoin.org/bitcoin.pdf",
+    content: "The whitepaper, by Satoshi Nakomoto, is a document explaining Bitcoin. ",
+    tags: ["Coco_Ardo","Blockchain","Transactions","document","Satoshi Nakamoto","Proof-of-Work","Proof of Work","POW","Verification","Verify","UTXO","UTXOs"]
+  },
+  {
+    title: "Lightning Whitepaper",
+    url: "https://lightning.network/lightning-network-paper.pdf",
+    content: "This whitepaper is a descriptive document explaining the Lightning Network",
+    tags: ["Coco_Ardo","Blockchain","Transactions","document"]
+  },
+  {
+    title: "What is a Bitcoin fullnode",
+    url: "https://armantheparman.com/6reasonsnode/",
+    content: "Why you should run your own Bitcoin fullnode",
+    tags: ["Coco_Ardo","privately","Privacy","Anonym","Pseudonym","Blockchain","Hardware","Verification","Verify"]
+  },
+
+//  Bitcoin functions
+  {
+    title: "Understanding transaction fees",
+    url: "https://www.youtube.com/watch?v=GN6PmCqFikw",
+    content: "What are transaction fees and how high should they be",
+    tags: ["Coco_Ardo","Blockchain","UTXO","UTXOs"]
+  },
+    {
+    title: "How to use coin control",
+    url: "https://thebitcoinmanual.com/articles/what-is-coin-control",
+    content: "What are UnspendTransactionOutputs (UTXOs) and how to use them",
+    tags: ["Coco_Ardo","Blockchain","transaction","UTXO","UTXOs"]
+  },
+    {
+    title: "How to use RBF and CPFP",
+    url: "https://web.archive.org/web/20241212135120/https://docs.samourai.io/en/wallet/features/miner-fees",
+    content: "What are ReplaceByFee and ChildPaysForParent",
+    tags: ["Coco_Ardo","Blockchain","transaction","UTXO","UTXOs","Replace by fee","Child pays for parent"]
+  },
+
+//  Bitcoin Guides/ Usecases
+  {
+    title: "Where can I spent my Bitcoins?",
+    url: "https://btcmap.org/map#3/0.00000/0.00000",
+    content: "A map of bitcoin-friendly merchants around the world.",
+    tags: ["Coco_Ardo","Buy","Purchase","Shop","Shops","Shopping","Stores","merchants","exchange","Map","Overview"]
+  },
+  {
+    title: "How to mine Bitcoins?",
+    url: "https://danheld.gay/",
+    content: "A overview of Bitcoin mining resources.",
+    tags: ["Coco_Ardo","danheld","ASIC","Pools","Pool","Solomining","Immersion","Equipment","Hardware","Heating","Proof-of-Work","Proof of Work","POW","Overview"]
+  },
+  {
+    title: "Learn to code",
+    url: "https://learning.chaincode.com/",
+    content: "A overview of Bitcoin related coding resources by chaincode.",
+    tags: ["Coco_Ardo","Programming","coding", "chaincode","Hacking","Lightning","Lightingnetwork","Python","Javascript","JS","Rust","C++","CPP","C#","HTLCs","Scripting","Application","Apps","Seminar","Course","Cource","Material","Development","Developing","Engeniering","Core","Verification","Verify","Overview"]
+  },
+  {
+    title: "Buy/sell Bitcoin without KYC",
+    url: "https://kycnot.me/?verification=verified&currency-mode=or&currencies=btc&currencies=fiat&currencies=cash&user-rating=4&min-score=8",
+    content: "A no-kyc exchange overview",
+    tags: ["Coco_Ardo","no-kyc","Purchase","exchange","privately","Privacy","Anonym","Pseudonym","Account","Overview"]
+  },
+
+//  Generel
+  {
+    title: "What is TOR?",
+    url: "https://skerritt.blog/how-does-tor-really-work/",
+    content: "The United States Naval Research Laboratory developed The Onion Routing Protocol (Tor) to protect U.S. intelligence communications online.",
+    tags: ["Coco_Ardo","The Onion Routing","Communications"]
+  },
+  {
+    title: "What is KYC?",
+    url: "https://www.investopedia.com/terms/k/knowyourclient.asp",
+    content: "Know Your Customer (KYC) is a standard that ensures advisors can verify a client's identity ",
+    tags: ["Coco_Ardo","Know Your Customer","privately","Privacy","Anonym","Pseudonym","Account"]
+  },
+  {
+    title: "What is PGP?",
+    url: "https://proton.me/blog/what-is-pgp-encryption",
+    content: "PGP is an encryption method that lets you communicate privately online.",
+    tags: ["Coco_Ardo","encryption","Message","Messaging","Communications","privately","Privacy","Anonym","Pseudonym"]
+  }
+];
+
+// Wichtige Begriffe:
+// onchain, wallet
