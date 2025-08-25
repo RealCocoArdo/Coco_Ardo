4388a64b2c7bae146ff2775afe6665dbf9501b3b
Coco_Ardo <105318683+RealCocoArdo@users.noreply.github.com>
2025-08-25 16:30:59 +0200

    change position

--- js/share.js
+++ js/share.js
@@ -4,15 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // HTML for button + Popup dynamic
   container.innerHTML = `
-    <button id="share-btn" style="position:fixed; bottom:20px; right:20px; padding:10px 15px; border-radius:8px; background:#232533; color:white; border:none; cursor:pointer; z-index:1000;">
-      Share
-    </button>
+    <div style="justify-content: center; display: flex; flex-direction: row;">
+      <button id="share-btn" style="background:#15202b; padding-right: 2.5%; padding-left: 2.5%; color:grey; justify-content: center; border:none; cursor:pointer;">
+        <h3>Share</h3>
+      </button>
+    </div>
 
     <div id="qr-popup" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background:rgba(0,0,0,0.6); justify-content:center; align-items:center; z-index:1001;">
       <div style="background:white; padding:20px; border-radius:12px; text-align:center; max-width:90%; box-shadow:0 4px 20px rgba(0,0,0,0.3);">
         <p style="color:black;">Scan the QR-Code:</p>
         <img id="qr-image" alt="QR-Code" style="width:200px; height:200px;" />
-        <p style="color:black;">CocoArdo.org/</p>
+        <br>
         <button id="qr-close" style="margin-top:10px; padding:6px 12px; border:none; background:#0077ff; color:white; border-radius:6px; cursor:pointer;">
           Close
         </button>
